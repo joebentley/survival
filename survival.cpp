a37cb758d3ca5d983b1f3e1a19552597f6dfd887
Joe Bentley <joebentley10@gmail.com>
2018-05-09 19:22:36 +0100

    Moved text stuff into font.h

--- survival.cpp
+++ survival.cpp
@@ -7,25 +7,6 @@
 #include "dialog.h"
 #include "world.h"
 
-const int CHAR_HEIGHT = 12;
-const int CHAR_WIDTH = 8;
-const int NUM_PER_ROW = 16;
-const std::string CHARS =
-    "space dwarf dwarf2 heart diamond club spade circle emptycircle ring emptyring male female note1 note2 gem "
-    "sloperight slopeleft updown alert pagemark sectionmark thickbottom updown2 up down right left boxbottomleft leftright slopeup slopedown "
-    "space2 ! \" # $ % & ' ( ) * + , - . / "
-    "0 1 2 3 4 5 6 7 8 9 : ; < = > ? "
-    "@ A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ \\ ] ^ _ "
-    "` a b c d e f g h i j k l m n o p q r s t u v w x y z { : } ~ triangle "
-    "accentC accentu accente accenta accenta2 accenta3 accenta4 accentc accente2 accente3 accente4 accenti accenti2 accenti3 accentA  accentA2 "
-    "accentE accentae accentAE accento accento2 accento3 accentu2 accentu3 accenty accentO accentU cent pound yen Pt function "
-    "accenta5 accenti4 accento4 accentu4 accentn accentN aoverbar ooverbar qmark2 boxtopleft boxtopright half quarter emark2 muchless muchgreater "
-    "shaded shaded2 shaded3 p1 p2 p3 p4 p5 p6 p7 p8 p9 p10 p11 p12 p13 "
-    "p14 p15 p16 p17 p18 p19 p20 p21 p22 p23 p24 p25 p26 p27 p28 p29 "
-    "p30 p31 p32 p33 p34 p35 p36 p37 p38 p39 p40 p41 p42 p43 p44 p45 "
-    "alpha beta Gamma Pi Sigma sigma mu tau Phi theta Omega delta inf ninf in intersect "
-    "equiv pm gteq lteq upperint lowerint div approx degree cdot hyphen sqrt endquote power2 block space3";
-
 //Screen dimension constants
 const int WINDOW_WIDTH = CHAR_WIDTH * SCREEN_WIDTH;
 const int WINDOW_HEIGHT = CHAR_HEIGHT * SCREEN_HEIGHT;
